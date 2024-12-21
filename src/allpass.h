ad7d9d278f1b3a4ecf616e651d8705915066f473
Björn Eriksson <martorn@gmail.com>
2024-12-21 22:25:19 +0100

    updated allpass

--- src/allpass.h
+++ src/allpass.h
@@ -1,48 +1,49 @@
+#pragma once
+
 #include <cstdint>
 
 namespace dsp {
-using UInt = uint16_t;
+  using UInt = uint16_t;
 
-template <UInt S> class allpass {
-public:
-  void init();
-  inline float process(const float in);
-  void setDelayTime(const float dtime) { m_delayTime = dtime;}
+  template<UInt S>
+  class allpass {
+  public:
+    allpass() = default;
 
-private:
-  uint16_t m_size = S;
-  float m_buf[S];
-  uint16_t m_pos;
-  float m_delayTime;
-  float m_yprev;
-  float m_xprev;
-};
+    void setDelayTime(const float dtime) { m_delayTime = dtime; }
 
-template <UInt S> void allpass<S>::init() {
-  m_pos = 0;
-  m_xprev = 0;
-  m_yprev = 0;
-  m_delayTime = static_cast<float>(m_size);
-}
+    void init() {
+      m_pos = 0;
+      m_xprev = 0;
+      m_yprev = 0;
+      m_delayTime = static_cast<float>(m_size);
+    }
 
-template <UInt S> inline float allpass<S>::process(const float in) {
+    inline float process(const float in) {
+      m_pos += 1;
 
-  m_pos += 1;
+      if (m_pos >= m_delayTime)
+        m_pos -= m_delayTime;
 
-  if (m_pos >= m_delayTime)
-    m_pos -= m_delayTime;
+      auto pos = static_cast<uint16_t>(m_pos);
+      const float frac = m_pos - static_cast<float>(pos);
+      const float aa = (1 - frac) / (1 + frac);
+      const float xn = m_buf[pos % m_size];
+      const float out = aa * (xn - m_yprev) + m_xprev;
+      m_xprev = xn;
+      m_yprev = out;
 
-  uint16_t pos = static_cast<uint16_t>(m_pos);
-  float frac = m_pos - pos;
-  float aa = (1 - frac) / (1 + frac);
-  float xn = m_buf[pos % m_size];
-  float out = aa * (xn - m_yprev) + m_xprev;
-  m_xprev = xn;
-  m_yprev = out;
+      m_buf[pos] = in;
 
-  m_buf[pos] = in;
+      return out;
+    }
 
-  return out;
-}
-
+  private:
+    uint16_t m_size = S;
+    float m_buf[S];
+    float m_pos = 0.f;
+    float m_delayTime = S;
+    float m_yprev = 0.f;
+    float m_xprev = 0.f;
+  };
 } // namespace dsp
